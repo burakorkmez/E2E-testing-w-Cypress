e8a72552a145c5219d607820a658b6c313f6f83a
Burak Örkmez <burakorkmezz@gmail.com>
2023-02-24 19:59:44 +0300

    Simulating keyboard typing

--- 1-core-features-and-syntax/cypress/e2e/tasks.cy.js
+++ 1-core-features-and-syntax/cypress/e2e/tasks.cy.js
@@ -12,4 +12,17 @@ describe("tasks management", () => {
 		cy.get(".backdrop").should("not.exist");
 		cy.get(".modal").should("not.exist");
 	});
+
+	it("should create a new task", () => {
+		cy.visit("http://localhost:5173/");
+		cy.contains("Add Task").click();
+		cy.get("#title").type("My new task");
+		cy.get("#summary").type("My new task summary");
+		cy.get(".modal").contains("Add Task").click();
+		cy.get(".task").should("have.length", 1);
+		cy.get(".task h2").contains("My new task");
+		cy.get(".task p").contains("My new task summary");
+		cy.get(".backdrop").should("not.exist");
+		cy.get(".modal").should("not.exist");
+	});
 });
