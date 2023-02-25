27fbed62d3dda339d4d7a2cbcb033f0fedb3a790
Burak Örkmez <burakorkmezz@gmail.com>
2023-02-25 17:15:03 +0300

    Validating form input

--- 2-element-selections-assertions-and-more/cypress/e2e/contact.cy.js
+++ 2-element-selections-assertions-and-more/cypress/e2e/contact.cy.js
@@ -5,6 +5,7 @@ describe("contact form", () => {
 		cy.get("[data-cy='contact-input-message']").type("Hello, Cypress!");
 		cy.get("[data-cy='contact-input-name']").type("John Doe");
 		cy.get("[data-cy='contact-input-email']").type("test@example.com");
+		// cy.get("[data-cy='contact-input-email']").type("test@example.com{enter}"); // submitting with enter key
 		cy.get("[data-cy='contact-btn-submit']").contains("Send Message").should("not.have.attr", "disabled");
 		cy.get("[data-cy='contact-btn-submit']").click();
 		cy.get("[data-cy='contact-btn-submit']").contains("Sending...").and("have.attr", "disabled");
@@ -20,5 +21,32 @@ describe("contact form", () => {
 		// cy.get("[data-cy='contact-btn-submit']").as("submitBtn");
 		// cy.get("@submitBtn").click();
 		// cy.get("@submitBtn").contains("Sending...").should("have.attr", "disabled");
+	});
+
+	it("should validate the form input", () => {
+		cy.visit("http://localhost:5173/about");
+		cy.get('[data-cy="contact-btn-submit"]').click();
+		cy.get('[data-cy="contact-btn-submit"]').then(($btn) => {
+			expect($btn).to.not.have.attr("disabled");
+			expect($btn.text()).to.not.equal("Sending...");
+		});
+		cy.get('[data-cy="contact-btn-submit"]').contains("Send Message");
+		cy.get("[data-cy='contact-input-message']").focus().blur();
+		cy.get("[data-cy='contact-input-message']")
+			.parent()
+			.should("have.attr", "class")
+			.and("match", /invalid/);
+
+		cy.get("[data-cy='contact-input-name']").focus().blur();
+		cy.get("[data-cy='contact-input-name']")
+			.parent()
+			.should("have.attr", "class")
+			.and("match", /invalid/);
+
+		cy.get("[data-cy='contact-input-email']").focus().blur();
+		cy.get("[data-cy='contact-input-email']")
+			.parent()
+			.should("have.attr", "class")
+			.and("match", /invalid/);
 	});
 });
