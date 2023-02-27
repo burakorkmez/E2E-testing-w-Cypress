b178d41f029e7365c4ccdf638b09042009e11bf0
Burak Örkmez <burakorkmezz@gmail.com>
2023-02-27 14:19:20 +0300

    3rd project folder added

---
+++ 3-configuration-and-customization/cypress/e2e/navigation.cy.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+describe("page navigation", () => {
+	it("should navigate between pages", () => {
+		cy.visit("http://localhost:5173/");
+		// instead of class, id and element selectors we can use data-cy attributes as it is more reliable and less prone to change
+		cy.get("[data-cy='header-about-link']").click();
+		cy.location("pathname").should("eq", "/about");
+		cy.go("back");
+		cy.location("pathname").should("eq", "/");
+		cy.get("[data-cy='header-about-link']").click();
+		cy.get("[data-cy='header-home-link']").click();
+		cy.location("pathname").should("eq", "/");
+	});
+});
